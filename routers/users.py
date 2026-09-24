6773cdf48cdddec96fdfe090b2f665998056e7c7
devamanas-t <devamanasthenari@gmail.com>
2026-09-24 08:57:26 +0530

    new api

--- routers/users.py
+++ routers/users.py
@@ -29,10 +29,21 @@ def get_all_users(db:Session=Depends(get_db)):
     db_user =db.query(User).all()
     return db_user
 
-
+#the varible contain input data should be same as the function parameter
+# eg user_data in @router and def get
 @router.get("/user/name/{user_data}",response_model=list[user.UserResponse])
 def get_user_by_name(user_data:str,db:Session=Depends(get_db)):
     db_user=db.query(User).filter(User.user_name == user_data).all()
+    if not db_user:
+        raise errorHandling()
+    return db_user
+
+@router.get("/user/phone/{user_data}",response_model=list[user.UserResponse])
+def get_user_by_phone(user_data:str,db:Session = Depends(get_db)):
+    if len(user_data) != 10:
+        print("not 10 numbers")
+        raise errorHandling()
+    db_user = db.query(User).filter(User.phone_no==user_data).all()
     if not db_user:
         raise errorHandling()
     return db_user
